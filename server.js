ecb2f536a244b75e766ee0e98a97ebc17ae9dca6
Uniliva Alves Pereira <uniliva-ap@hotmail.com>
2018-01-25 07:56:56 -0200

    montando estrutura para  subi aplicação no Heroku

---
+++ server.js
@@ -0,0 +1,10 @@
+//este arquivo serve para fazer o deploy dessa aplicação no heroku
+//Install express server
+const express = require('express');
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/'));
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
