4c0e52eea7c1d9d33a7b1f56b42173f0db51910c
Uniliva Alves Pereira <uniliva-ap@hotmail.com>
2018-02-15 15:24:36 -0200

    Alterando o servicor

--- js/config/config.js
+++ js/config/config.js
@@ -1,4 +1,4 @@
 angular.module("uConta").value("config",{
 	//baseUrl:"http://localhost:8070"
-	baseUrl:"http://fatecexpo.fatecosasco.com.br:2651/rest"
+	baseUrl:"https://ucontas-service.herokuapp.com"
 });
